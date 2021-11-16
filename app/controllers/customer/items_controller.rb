63e857a1e5fe70b50dd5fe0ce43846a60b444cab
wak10 <taka.watagashi422@gmail.com>
2021-11-16 13:24:42 +0000

    [add]商品一覧画面

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -1,5 +1,12 @@
 class Customer::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page]).per(10)
   end
 
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre, :name, :image, :introduction, :price, :is_active )
+  end
 end
